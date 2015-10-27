c8713aa246d95cba29500efda046388b6b85819c
jan fajfr <jan@rfq-hub.com>
2015-10-27 15:36:08 +0100

    adding the migration script for further reference and some notes to the draft about option pricing

--- _drafts/2015-10-14-fsharp-options-pricing.md
+++ _drafts/2015-10-14-fsharp-options-pricing.md
@@ -1,3 +1,102 @@
 Pricer is a small library for pricing options and generating payoff charts.
 
 Payoff charts show you the profit of an option based on the movement of the underlying security itself. You can create a payoff charts of any derivative (options, futures, convertible bonds). Multiple options traded in the same time form strategies which can be used to obtain different type of protection or leverage against the movements of the underlying stock.
+
+Binomial pricing
+----------------
+
+Imperative way:
+//we need to construct the binomial pricing model, using the CRR (Cox, Ross and Rubinstein)
+//the original model is composed of 3 parameters p,u,d.
+//u - up probability, d - down probability. p is the technical probability
+//here we have PUp and PDown, for further simplifacation of the calculations
+
+```FSharp
+let deltaT = option.TimeToExpiry/float steps
+let up = exp(stock.Volatility*sqrt deltaT)
+let down = 1.0/up
+let R = exp (stock.Rate*deltaT)
+let p_up = (R-down)/(up-down)
+let p_down = 1.0 - p_up
+
+let pricing = {
+    Periods = steps
+    Up = up
+    Down = down
+    PUp = p_up
+    PDown = p_down
+    Rate = R
+    Option = option
+    Ref = stock.CurrentPrice
+}
+
+let prices = Array.zeroCreate pricing.Periods
+let optionValue =
+    match pricing.Option.Kind with
+            | Call -> fun i -> max 0.0 (prices.[i] - pricing.Option.Strike)
+            | Put -> fun i -> max 0.0 (pricing.Option.Strike - prices.[i])
+
+prices.[0] <- pricing.Ref*(pricing.Down**(float pricing.Periods))
+let oValues = Array.zeroCreate pricing.Periods
+oValues.[0]<- optionValue 0
+for i in 1 ..pricing.Periods-1 do
+    prices.[i] <- prices.[i-1]*pricing.Up*pricing.Up
+    oValues.[i]<- optionValue i
+
+let counter = pricing.Periods-2
+for step = counter downto 0 do
+    for j in 0 .. step do
+        oValues.[j] <- (pricing.PUp*oValues.[j+1]+pricing.PDown*oValues.[j])*(1.0/pricing.Rate)
+oValues.[0]
+```
+
+Functional way
+--------------
+The basic step is here:
+
+```FSharp
+let step (derPrice:float list) (pricing:BinomialPricing) =
+    derPrice  |> Seq.pairwise
+              |> Seq.fold (fun derPrice' (down,up)->
+                let price' = (pricing.PUp*up+pricing.PDown*down)*(1.0/pricing.Rate)
+                derPrice' @ [price'])[]
+
+let rec reducePrices (derPrice:float list) (pricing:BinomialPricing) =
+    match derPrice with
+            | [single] -> single
+            | prices -> reducePrices (step prices pricing) pricing
+```
+
+The **step** method takes a list of prices, which are the prices of the option in the depth n of the pricing tree. If the tree has 4 steps, then in the last time step (at the maturity), this list will have 4 members. Step method takes this list and moves it one time step up in the tree. So the output of this method should have 3 elements.
+
+We can profit of F# **pairwise** method to iterate over tuples of consecutive elements.
+
+Rewriting all in functional way:
+```FSharp
+let binomialPricingFunc (pricing:BinomialPricing) =
+    let optionVal stock =
+        match pricing.Option.Kind with
+                | Call -> max 0.0 (stock - pricing.Option.Strike)
+                | Put -> max 0.0 (pricing.Option.Strike - stock)
+
+    let prices = generateEndNodePrices pricing.Ref pricing.Up pricing.Periods optionVal
+
+    let step (prices:(float*float) list) =
+        prices
+            |> Seq.pairwise
+            |> Seq.fold (fun prices' ((sDown,dDown),(sUp,dUp)) ->
+                let derValue = (pricing.PUp*dUp+pricing.PDown*dDown)*(1.0/pricing.Rate)
+                let stockValue = sUp*pricing.Down
+                let der' = if pricing.Option.Style = American then
+                                let prematureExValue = optionVal stockValue
+                                max derValue prematureExValue
+                            else derValue
+                prices' @ [stockValue,der'])[]
+
+    let rec reducePrices prices =
+        match prices with
+                | [(stock,der)] -> der
+                | prs -> reducePrices (step prs)
+
+    reducePrices prices
+```
---
+++ scripts/migration.fsx
@@ -0,0 +1,106 @@
+open System.IO
+open System.Text
+open System.Text.RegularExpressions
+
+let toReplace = [
+    (@"\*\\","")
+    (@"\\(\n|\r|\r\n)","")
+    ("{.brush:.*csharp}","")
+    ("{.brush:.*java}","")
+    ("{.brush:.*xml}","")
+    ("<div(.*)>","")
+    ("</div>","")
+    ("</?span(.*)>","")
+    ("%20","")
+    ("{.prettyprint}","")
+    ("%}\)","}\)")
+    ("{%post","{post")
+    ("{.brush:prettyprint}","")
+    (" {.prittyprint}","")
+    (" {.brush: .py; .auto-links: .true; .collapse: .false; .first-line: .1; .gutter: .true; .html-script: .false; .light: .false; .ruler: .false; .smart-tabs: .true; .tab-size: .4; .toolbar: .true;}","")
+]
+
+let startProcess fileName output=
+    let p = new System.Diagnostics.Process();
+
+    p.StartInfo.FileName <- "pandoc";
+    p.StartInfo.Arguments <- sprintf "-f html -t markdown %s -o %s" fileName output
+    p.StartInfo.UseShellExecute <- false
+    p.Start() |> ignore
+    p.WaitForExit()
+
+let createMdFilesFromHtml files =
+    files |> Seq.iter (fun filename ->
+        let content = File.ReadAllText(filename)
+        let outputFile = filename.Replace(".html",".md")
+        let yamlHeader = Regex.Match(content,"(?s)---\s.*---")
+
+        if yamlHeader.Success then
+            printfn "Found header: %s" yamlHeader.Value
+            let withtoutHeader = Regex.Replace(content,"(?s)---.*---","")
+
+            let modifiedHtml = filename.Replace(".html","_modified.html")
+            File.WriteAllText(modifiedHtml,withtoutHeader)
+
+            startProcess modifiedHtml outputFile
+            let mdContent = File.ReadAllText(outputFile)
+
+            let replaced = toReplace |> Seq.fold (fun v acc ->
+                let testMatch = Regex.Match(v,fst(acc))
+                if testMatch.Success then
+                    printfn "Found: %s" testMatch.Value
+                Regex.Replace(v,fst(acc),snd(acc))) mdContent
+
+            let withHeader = yamlHeader.Value + "\n" + replaced
+
+            File.WriteAllText(outputFile,withHeader)
+            File.Delete filename
+            File.Delete modifiedHtml
+        else
+            printfn "Header not found"
+    )
+
+
+let generateLinkingTemplateCode filesDirectory =
+
+    let files = Directory.GetFiles(filesDirectory, ".html",SearchOption.AllDirectories)
+
+    let addToTemplate acc filename =
+        let parsedValues = Regex.Match(filename,"(\d{4})-(\d{2})-(\d{2})-(.*).html")
+        let year = parsedValues.Groups.[1].Value
+        let month = parsedValues.Groups.[2].Value
+        let name = parsedValues.Groups.[4].Value
+
+        printfn "Year: %s month: %s name:%s" year month name
+
+        let originalUrl = sprintf "http://hoonzis.blogspot.com/%s/%s/%s.html" year month name
+        let newUrl = sprintf "http://www.hoonzis.com/%s" name
+
+        let ifStatement = match acc with
+                            | "" -> "<b:if",""
+                            | _ -> "<b:elseif","/"
+
+
+        let template = sprintf "%s cond='data:blog.canonicalUrl == \"%s\"'%s>\n\t<link rel=\"canonical\" href=\"%s\"/>\n\t<meta http-equiv=\"refresh\" content=\"0; url=%s\"/>\n" (fst ifStatement) originalUrl (snd ifStatement) newUrl newUrl
+        sprintf "%s\n%s" acc template
+
+    let templatesPrefix = sprintf "%s\n</b:if>" (files |> Seq.fold addToTemplate "")
+    File.WriteAllText(@"C:\\temp\\xmlprefix.xml",templatesPrefix)
+
+
+let filesDirectory = "d:\\projects\\hoonzis.github.io\\bloggerfiles"
+let files = Directory.GetFiles(filesDirectory)
+let geturlmap acc filename =
+    let parsedValues = Regex.Match(filename,"(\d{4})-(\d{2})-(\d{2})-(.*).html")
+    let year = parsedValues.Groups.[1].Value
+    let month = parsedValues.Groups.[2].Value
+    let name = parsedValues.Groups.[4].Value
+
+    printfn "Year: %s month: %s name:%s" year month name
+
+    let originalUrl = sprintf "http://hoonzis.blogspot.com/%s/%s/%s.html" year month name
+    let newUrl = sprintf "http://www.hoonzis.com/%s" name
+    sprintf "%s\n%s, %s" acc originalUrl newUrl
+
+let completeMap = files |> Seq.fold (geturlmap) ""
+File.WriteAllText(@"C:\\temp\\urlmap.csv",completeMap)
